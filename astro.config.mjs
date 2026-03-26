69511669e1b84be90af1c062b56cecad163c3189
a-put <58515331+a-put@users.noreply.github.com>
2026-03-26 13:31:21 +0300

    Update site config for custom domain putintsev.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://a-put.github.io',
-  base: '/cv-profile-astro',
+  site: 'https://putintsev.io',
+  base: '/',
   vite: {
     plugins: [tailwindcss()]
   }
